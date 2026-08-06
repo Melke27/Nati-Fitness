639b60ffa4d894d8550c01a8dae1e66a55ea4d34
Melke27 <melkamuwako5@gmail.com>
2026-08-06 10:09:33 +0300

    Clean public page: align colors to spec, trim homepage to 6 sections

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,7 @@
 import { Hero } from '@/components/home/Hero'
 import { TrustBar } from '@/components/home/TrustBar'
-import { About } from '@/components/home/About'
 import { ProgramsSection } from '@/components/home/ProgramsSection'
-import { ServicesSection } from '@/components/home/ServicesSection'
-import { WhyChoose } from '@/components/home/WhyChoose'
 import { Transformations } from '@/components/home/Transformations'
-import { Process } from '@/components/home/Process'
-import { Gallery } from '@/components/home/Gallery'
 import { Testimonials } from '@/components/home/Testimonials'
 import { PricingSection } from '@/components/home/PricingSection'
 import { CTABand } from '@/components/home/CTABand'
@@ -16,13 +11,8 @@ export default function Home() {
     <>
       <Hero />
       <TrustBar />
-      <About />
       <ProgramsSection limit={6} />
-      <ServicesSection />
-      <WhyChoose />
       <Transformations />
-      <Process />
-      <Gallery />
       <Testimonials />
       <PricingSection />
       <CTABand />
